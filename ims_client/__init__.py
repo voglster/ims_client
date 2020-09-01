811a87f22d1526e071db43cb9930d0083bc42732
Zeke Zumbro <zeke.zumbro@capspire.com>
2020-09-01 13:56:17 -0500

    Bump version: 0.1.5 → 0.1.6

--- ims_client/__init__.py
+++ ims_client/__init__.py
@@ -1,6 +1,6 @@
 """Simple python client for the capspire IMS Server"""
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 from datetime import datetime
 from functools import lru_cache
 from os import getenv
